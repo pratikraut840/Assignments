87f78a77275fce8b9b5c39f94da817e21fc4879b
Pratik Raut <pratikraut840@gmail.com>
2025-12-13 17:23:39 +0530

    Assignment 3 Completed

---
+++ src/assignments/Assignment3_EmpGroups.java
@@ -0,0 +1,30 @@
+package assignments;
+
+public class Assignment3_EmpGroups {
+	
+	public static void main(String[] args) {
+		
+		//Create the object of parentClass & Retrieve the data from the Employees class,
+		Assignment3_Employees empObj = new Assignment3_Employees();
+		
+		//Create another class called EmpGroups that contains two arrays to store the employee names and IDs.
+		String[] empNames = new String[3];
+		short[] empIds = new short[3];
+		
+		//store it in these arrays
+		empNames[0] = empObj.emp1Name;
+		empNames[1] = empObj.emp2Name;
+		empNames[2] = empObj.emp3Name;
+		
+		empIds[0] = empObj.emp1Id;
+		empIds[1] = empObj.emp2Id;
+		empIds[2] = empObj.emp3Id;
+		
+		System.out.println("Employee Name: "+empNames[0]+" , "+"Employee ID: "+empIds[0]);
+		System.out.println("Employee Name: "+empNames[1]+" , "+"Employee ID: "+empIds[1]);
+		System.out.println("Employee Name: "+empNames[2]+" , "+"Employee ID: "+empIds[2]);
+
+
+	}
+
+}
---
+++ src/assignments/Assignment3_Employees.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class Assignment3_Employees {
+//	Create an Employees class that stores the following employee names and IDs in separate variables:
+	
+	//Employee Names: "Bharath", "John", "Aarav"
+	String emp1Name = "Bharath";
+	String emp2Name = "John";
+	String emp3Name = "Aarav";
+	
+	//Employee IDs: 1234, 1235, 1236
+	short emp1Id = 1234; 
+	short emp2Id = 1235; 
+	short emp3Id = 1236; 
+
+	public static void main(String[] args) {
+
+	}
+
+}
