73f54a87157e750da5a04a914ba0a9d31b8a3732
Pratik Raut <pratikraut840@gmail.com>
2025-12-28 14:43:20 +0530

    Assignment 12 ReCompleted

--- src/assignments/Assignment13_xRaiseToPowerN.java
+++ src/assignments/Assignment13_xRaiseToPowerN.java
@@ -2,33 +2,29 @@ package assignments;
 
 public class Assignment13_xRaiseToPowerN {
 
-	public static double calculateXPower(double x, int n) {
+	public static double calculateXPower(double number, int power) {
 
-		// if any number raised to power 0 is 1 and return 1.0
-		if (n == 0) {
+		// if any number raised to power 0 is 1 then return 1.0
+		if (power == 0) {
 			return 1.0;
 		}
 
-		long exp = n; // store n in a long
-
-		// Checks if the exponent is negative and convert negative power into positive
-		if (exp < 0) {
-			x = 1 / x;
-			exp = -exp;
+		// Checks if the exponent is negative then convert negative power into positive
+		if (power < 0) {
+			number = 1 / number;
+			power = -power;
 		}
 
-		double result = 1.0; // initialize result
-		for (long i = 0; i < exp; i++) {
-			result = result * x;
+		double result = 1.0; // initial result
+		for (int i = 0; i < power; i++) {
+			result = result * number;
 		}
 		// Returns the final computed power
 		return result;
 	}
 
 	public static void main(String[] args) {
-
 		System.out.println(calculateXPower(5, 3));
-
 	}
 
 }
